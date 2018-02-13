c3627debe7c1fde3f0645fe186e8af365ebfa8b0
Sebastian Jachec <sjachec1@gmail.com>
2018-02-13 23:36:30 +0000

    Modularised processImage() function into several others, add config.json template file to set custom source folder [breaking changes]

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const { exec } = require('child_process')
 const fetch = require('node-fetch')
 const cheerio = require('cheerio')
 const colors = require('colors')
+const config = require('config')
 
-const occurrences = (string, subString, allowOverlapping) => {
+const countOccurrences = (string, subString, allowOverlapping) => {
   string += "";
   subString += "";
   if (subString.length <= 0) return (string.length + 1);
@@ -25,87 +26,120 @@ const occurrences = (string, subString, allowOverlapping) => {
   return n;
 }
 
+const deleteFile = path => {
+  fs.unlink(path, () => { });
+}
+
+const parseTesseractOutput = outputPath => {
+  const contents = fs.readFileSync(outputPath, 'utf8')
+  deleteFile(outputPath)
+
+  // Filter non-empty/non-blank lines
+  const lines = contents.split('\n').filter(x => x && x.length > 1);
+
+  const questionLines = lines.slice(0, lines.length - 1);
+  const title = questionLines.slice(0, questionLines.length - 3).join(' ');
+  const isInvertedQuestion = title.toLowerCase().indexOf(' not ') >= 0;
+  const choices = questionLines.slice(questionLines.length - 3, questionLines.length);
+
+  return {
+    title,
+    isInvertedQuestion,
+    choices
+  }
+}
+
+const parseGoogleSearchResults = (questionChoices, isInvertedQuestion, results) => {
+  const $ = cheerio.load(results[0]);
+  const genericSearch = results[0].toLowerCase();
+
+  const choicesWithCounts = questionChoices.map(o => ({ name: o }));
+
+  for (let i = 0; i < choicesWithCounts.length; ++i) {
+    const result = results[i + 1];
+    const $$ = cheerio.load(result);
+    choicesWithCounts[i].scopedCount = parseInt($$('#resultStats').text().toLowerCase().replace('about ', '').replace(' results', '').replace(/,/g, ''), 10);
+  }
+
+  const sortedChoices = choicesWithCounts
+    .map(o => ({ ...o, count: countOccurrences(genericSearch, o.name.toLowerCase(), true) }))
+    .sort((o1, o2) => {
+      if (o1.count === o2.count) {
+        return o1.scopedCount > o2.scopedCount ? -1 : 1;
+      }
+      
+      return o1.count > o2.count ? -1 : 1
+    });
+
+  if (isInvertedQuestion) {
+    sortedChoices.reverse();
+  }
+
+  console.log(isInvertedQuestion ? colors.bgRed(colors.white('\nQuestion has NOT - reversing results\n')) : '');
+  if (sortedChoices && sortedChoices.length > 0) {
+    console.log(colors.bgBlack(colors.green(`Top Answer: ${sortedChoices[0].name} (${sortedChoices[0].count} - ${sortedChoices[0].scopedCount})`)));
+  }
+
+  console.log()
+  const resultSections = $('div.g');
+  const first = resultSections.first();
+  const next = first.next();
+  const third = next.next();
+  const four = third.next();
+  const five = four.next();
+
+  console.log('Raw Google results:')
+
+  [first, next, third].forEach(x => {
+    //console.log(`\t${x.find('h3.r').text()} [${x.find('cite').text().substring(0, 50)}...]`)
+    x.find('span.st').text().split('\n').forEach(y => {
+      //console.log(`\t\t${y}`);
+    });
+    //console.log();
+  });
+
+  console.log(JSON.stringify(sortedChoices, null, 2));
+}
+
+const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36';
+
 const processImage = path => {
   console.log('\033c')
   console.log('Processing...');
+
   exec(`tesseract "${path}" "${path}.log"`, (err, stdout, stderr) => {
-    //fs.unlink(`${path}`, () => { });
+    // Delete the screenshot
+    deleteFile(path)
+
     if (err) {
       // node couldn't execute the command
-      console.log(`err: ${err}`);
-      return;
+      console.log(`Tesseract OCR error: ${err}`)
+      return
     }
 
-    const contents = fs.readFileSync(`${path}.log.txt`, 'utf8');
-    // Delete log file
-    fs.unlink(`${path}.log.txt`, () => { });
-
-    const lines = contents.split('\n').filter(x => x && x.length > 1);
-    const questionLines = lines.slice(0, lines.length - 1);
-    const title = questionLines.slice(0, questionLines.length - 3).join(' ');
-    const shouldInvert = title.toLowerCase().indexOf(' not ') >= 0;
-    const options = questionLines.slice(questionLines.length - 3, questionLines.length);
-    const searchTitle = shouldInvert ? title.replace(/ not /i, ' ') : title;
+    const { title, isInvertedQuestion, choices } = parseTesseractOutput(`${path}.log.txt`)
+    
+    const searchTitle = isInvertedQuestion ? title.replace(/ not /i, ' ') : title;
 
     console.log(`${colors.gray('Got question:')} ${title}`);
-    console.log(`${colors.gray('Got options:')}  ${options.join(', ')}`);
+    console.log(`${colors.gray('Got choices:')}  ${choices.join(', ')}`);
 
-    const headers = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36' };
-    const promises = ['', ...options].map(o => fetch(`https://www.google.com/search?q=${encodeURIComponent(searchTitle)}+${encodeURIComponent('"' + o + '"')}`, { headers }));
-    Promise.all(promises.map(p => p.then(res => res.text()))).then((results) => {
-      const $ = cheerio.load(results[0]);
-      const genericSearch = results[0].toLowerCase();
+    const searchHeaders = { 'User-Agent': USER_AGENT };
 
-      const optionsWithCounts = options.map(o => ({ name: o }));
-      for (let i = 0; i < optionsWithCounts.length; ++i) {
-        const result = results[i + 1];
-        const $$ = cheerio.load(result);
-        optionsWithCounts[i].scopedCount = parseInt($$('#resultStats').text().toLowerCase().replace('about ', '').replace(' results', '').replace(/,/g, ''), 10);
-      }
+    const baseUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTitle)}+${encodeURIComponent('"' + o + '"')}`;
 
-      const sortedOptions = optionsWithCounts
-        .map(o => ({ ...o, count: occurrences(genericSearch, o.name.toLowerCase(), true) }))
-        .sort((o1, o2) => {
-          if (o1.count === o2.count) {
-            return o1.scopedCount > o2.scopedCount ? -1 : 1;
-          }
-          return o1.count > o2.count ? -1 : 1
-        });
-
-      if (shouldInvert) {
-        sortedOptions.reverse();
-      }
-
-      console.log(shouldInvert ? colors.bgRed(colors.white('\nQuestion has NOT - reversing results\n')) : '');
-      if (sortedOptions && sortedOptions.length > 0) {
-        console.log(colors.bgBlack(colors.green(`Top Answer: ${sortedOptions[0].name} (${sortedOptions[0].count} - ${sortedOptions[0].scopedCount})`)));
-      }
-      
-      console.log();
-      const resultSections = $('div.g');
-      const first = resultSections.first();
-      const next = first.next();
-      const third = next.next();
-      const four = third.next();
-      const five = four.next();
-
-      console.log('Raw Google results:');
-      [first, next, third].forEach(x => {
-        //console.log(`\t${x.find('h3.r').text()} [${x.find('cite').text().substring(0, 50)}...]`)
-        x.find('span.st').text().split('\n').forEach(y => {
-          //console.log(`\t\t${y}`);
-        });
-        //console.log();
-      });
-
-      console.log(JSON.stringify(sortedOptions, null, 2));
-    });
+    const promises = ['', ...choices].map(choice => fetch(baseUrl, { headers }));
+    
+    Promise.all(
+      promises.map(p => p.then(res => res.text()))
+    ).then(results => parseGoogleSearchResults(results));
   });
 }
 
 const username = os.userInfo().username
+const watchPath = config.watchPath || `/Users/${username}/Desktop`
 
-let watcher = chokidar.watch(`/Users/${username}/Desktop`, {
+let watcher = chokidar.watch(watchPath, {
   ignored: /(^|[\/\\])\../,
   ignoreInitial: true,
   persistent: true
